2a5db7ea28a1b86e463021ded1ca9e103b295045
pageshih <a0981863141@gmail.com>
2021-05-21 16:43:19 +0800

    Add files via upload

--- loginJS.js
+++ loginJS.js
@@ -1,47 +1,85 @@
-const api_path = 'kana';
-const baseUrl = 'https://vue3-course-api.hexschool.io';
+
+
+// const baseUrl = 'https://vue3-course-api.hexschool.io';
 const userName = document.querySelector('#username');
 const password = document.querySelector('#password');
 const loginBtn = document.querySelector('.btn-lg');
 
-loginBtn.addEventListener('click',function(e){
-  e.preventDefault();
-login();
-})
 
 
-function login(){
- let url = `${baseUrl}/admin/signin`;
-const user = {
-  "username": userName.value,
-  "password": password.value
-};
-axios.post(url, user)
-  .then((res)=>{
-    console.log(res.data);
-    if(res.data.success==true){
-      const token = res.data.token;
-      const expired = res.data.expired;
-      document.cookie = `logToken=${token}; expires=${new Date(expired)}; path=/`;
-      window.location.href = "productList.html";
-      return;
-    }
+const logInMethods = {
+  data:{
+    baseUrl: 'https://vue3-course-api.hexschool.io',
+  },
+  logInBtnEvent(){
+    loginBtn.addEventListener('click',(e)=>{
+      e.preventDefault();
+      let user = {
+        "username": `${userName.value}`,
+        "password": `${password.value}`
+        }
+      this.login(user);
     })
+  },
+  login(user){
+    axios.post(`${this.data.baseUrl}/admin/signin`, user)
+    .then((res)=>{
+      if(res.data.success==true){
+        const token = res.data.token;
+        const expired = res.data.expired;
+        document.cookie = `logToken=${token}; expires=${new Date(expired)}; path=/`;
+        window.location.href = "productList.html";
+        return;
+      }
+      })
+  },
+  init(){
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)logToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+    axios.defaults.headers.common['Authorization'] = token;
+    
+    axios.post(`${this.data.baseUrl}/api/user/check`)
+    .then((res)=>{
+      if(res.data.success == true){
+        window.location.href = "productList.html";
+        return;
+      }
+    })
+    }
 }
-isLogIn();
-function isLogIn(){
-const token = document.cookie.replace(/(?:(?:^|.*;\s*)logToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
-// console.log(token);
-axios.defaults.headers.common['Authorization'] = token;
 
-axios.post(`${baseUrl}/api/user/check`)
-.then((res)=>{
-  if(res.data.success == true){
-    window.location.href = "productList.html";
-    return;
-  }
-})
-}
+logInMethods.init();
+logInMethods.logInBtnEvent();
+// function login(){
+//  let url = `${baseUrl}/admin/signin`;
+// const user = {
+//   "username": userName.value,
+//   "password": password.value
+// };
+// axios.post(url, user)
+//   .then((res)=>{
+//     console.log(res.data);
+//     if(res.data.success==true){
+//       const token = res.data.token;
+//       const expired = res.data.expired;
+//       document.cookie = `logToken=${token}; expires=${new Date(expired)}; path=/`;
+//       window.location.href = "productList.html";
+//       return;
+//     }
+//     })
+// }
+// function isLogIn(){
+// const token = document.cookie.replace(/(?:(?:^|.*;\s*)logToken\s*\=\s*([^;]*).*$)|^.*$/, "$1"); 
+// // console.log(token);
+// axios.defaults.headers.common['Authorization'] = token;
+
+// axios.post(`${baseUrl}/api/user/check`)
+// .then((res)=>{
+//   if(res.data.success == true){
+//     window.location.href = "productList.html";
+//     return;
+//   }
+// })
+// }
  
 // function getProductList(){
   
